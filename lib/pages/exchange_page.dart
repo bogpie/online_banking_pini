0e946aaa6250ed1e71c96d350281b00d16779474
Bogdan Piele <bogdanpielemail@gmail.com>
2022-05-26 20:15:37 +0300

    Improved exchange page

--- lib/pages/exchange_page.dart
+++ lib/pages/exchange_page.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 class ExchangePage extends StatefulWidget {
@@ -11,6 +9,17 @@ class ExchangePage extends StatefulWidget {
 
 class _ExchangePageState extends State<ExchangePage> {
   String sellCurrency = "RON";
+  double sellValue = 0;
+  String buyCurrency = "RON";
+  double buyValue = 0;
+  Map<Map<String, String>, int> conversionMap = {
+    {"RON": "EUR"}: 1,
+    {"RON": "USD"}: 1,
+    {"EUR": "USD"}: 1,
+    {"EUR": "RON"}: 1,
+    {"USD": "RON"}: 1,
+    {"USD": "EUR"}: 1,
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -23,44 +32,96 @@ class _ExchangePageState extends State<ExchangePage> {
           const SizedBox(
             height: 16,
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: DropdownButtonHideUnderline(
-                    child: Card(
-                      elevation: 5,
-                      child: DropdownButton<String>(
-                        value: sellCurrency,
-                        icon: const Icon(Icons.arrow_downward),
-                        onChanged: (String? newValue) {
-                          setState(
-                            () {
-                              sellCurrency = newValue!;
-                            },
+          Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: DropdownButtonHideUnderline(
+                  child: Card(
+                    elevation: 5,
+                    child: DropdownButton<String>(
+                      value: sellCurrency,
+                      icon: const Icon(Icons.arrow_downward),
+                      onChanged: (String? newValue) {
+                        setState(
+                          () {
+                            sellCurrency = newValue!;
+                          },
+                        );
+                      },
+                      items: <String>['RON', 'EUR', 'USD']
+                          .map<DropdownMenuItem<String>>(
+                        (String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text("Selling $value"),
+                            ),
                           );
                         },
-                        items: <String>['RON', 'EUR', 'USD']
-                            .map<DropdownMenuItem<String>>(
-                          (String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text("Selling $value"),
-                            );
-                          },
-                        ).toList(),
-                      ),
+                      ).toList(),
                     ),
                   ),
                 ),
-                const Expanded(
-                  flex: 2,
-                  child: TextField(),
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              const Expanded(
+                flex: 3,
+                child: TextField(
+                  keyboardType: TextInputType.number,
                 ),
-              ],
-            ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: DropdownButtonHideUnderline(
+                  child: Card(
+                    elevation: 5,
+                    child: DropdownButton<String>(
+                      value: buyCurrency,
+                      icon: const Icon(Icons.arrow_downward),
+                      onChanged: (String? newValue) {
+                        setState(
+                          () {
+                            buyCurrency = newValue!;
+                          },
+                        );
+                      },
+                      items: <String>['RON', 'EUR', 'USD']
+                          .map<DropdownMenuItem<String>>(
+                        (String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text("Buying $value"),
+                            ),
+                          );
+                        },
+                      ).toList(),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              const Expanded(
+                flex: 3,
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                ),
+              ),
+            ],
           )
         ],
       ),
