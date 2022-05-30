c8310b28918942e3e5feb0964159d439c2ac7ee5
MarianNoaghea <noaghea.marian@gmail.com>
2022-05-30 20:31:29 +0300

    progress at the user details page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import '../services/user_data.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -9,6 +10,8 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  Map data = {};
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +28,55 @@ class _ProfilePageState extends State<ProfilePage> {
                   Navigator.pushNamed(context, '/');
                 },
                 child: const Text('Logout')),
-          ],
+            FutureBuilder(
+              future: getUserMap(FirebaseAuth.instance.currentUser!.uid)
+                  .then((result) => data = result),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          'First Name:  '
+                              '${data['firstName']} ',
+                          textScaleFactor: 2,
+                        ),
+                        Text(
+                          'Last Name:  '
+                              '${data['lastName']} ',
+                          textScaleFactor: 2,
+                        ),
+                        Text(
+                          'You have '
+                              '${data['currencies']['RON']} RON',
+                          textScaleFactor: 2,
+                        ),
+                        Text(
+                          'You have ${data['currencies']['EUR']} EUR',
+                          textScaleFactor: 2,
+                        ),
+                        Text(
+                          'You have ${data['currencies']['USD']} USD',
+                          textScaleFactor: 2,
+                        ),
+                        const SizedBox(height: 32),
+                        IconButton(
+                            onPressed: () {
+                              setState(() {});
+                            },
+                            icon: const Icon(Icons.refresh))
+                      ],
+                    ),
+                  ),
+                );
+              },
+            )
+          ]
+          ,
         ),
       ),
     );
