3ef187b1eadbcd9393ee61ac368558ef49e6cab7
Catalin Iovita <cataliniovita@yahoo.com>
2022-05-31 17:45:04 +0300

    Finish history transaction implementing

--- lib/pages/history_transaction.dart
+++ lib/pages/history_transaction.dart
@@ -63,19 +63,28 @@ class _TransactionHistory extends State<TransactionHistory> {
                                   ),
                                   IconButton(
                                       onPressed: () async {
-                                        /* Delete the transactions from database of
-                                        * both users -> sender and receiver */
-
                                         /* Delete the current user transaction */
+                                        String senderIbanCode =
+                                            data['transfers'][index]['iban'];
+
+                                        String currency =
+                                            data['transfers'][index]['currency'];
+
+                                        double amount =
+                                            data['transfers'][index]['amount'];
+
+                                        String transfer_id = "";
                                         List<dynamic>? currentTransfers = data["transfers"];
+                                        Map dataSend = await getUserMap(FirebaseAuth.instance.currentUser!.uid);
+                                        Map currenciesSend = dataSend["currencies"];
+
                                         if (currentTransfers != null) {
-                                          String transfer_id = "";
-
                                           /* Grab the common transfer_id */
-                                          transfer_id =
-                                          data["transfers"][index]["transfer_id"];
-                                          currentTransfers.removeAt(index);
+                                          transfer_id = data["transfers"][index]["transfer_id"];
 
+                                          /* Modify the currency and type of trans */
+                                          currentTransfers[index]["type"] = "sent";
+                                          currenciesSend[currency] += amount;
 
                                           /* Get instance of db of the current user*/
                                           DatabaseReference currentRef = FirebaseDatabase
@@ -86,61 +95,134 @@ class _TransactionHistory extends State<TransactionHistory> {
                                           );
 
                                           /* Update the new transfer list */
-                                          currentRef.update(
-                                              {"transfers": currentTransfers});
-
-                                          // Get the Sender Data and remove his transfer
-                                          /* Get the sender Iban to match with the uid */
-                                          String senderIbanCode =
-                                          data['transfers'][index]['iban'];
-
-                                          String senderUID = await
-                                          ibanCodeToUid(
-                                              senderIbanCode.substring(5, 9));
-
-                                          // /* Get the sender data */
-                                          Map senderData = await getUserMap(
-                                              senderUID);
-                                          //
-                                          List<dynamic>? senderTransfers = senderData["transfers"];
-                                          // if (senderTransfers != null && currentTransfers != null) {
-                                          //     /* Grab the specific transfer with common transfer_id */
-                                          //     if (transfer_id != "") {
-                                          //       print(transfer_id);
-                                          //       int indexRemove = senderTransfers
-                                          //           .indexOf(
-                                          //           transfer_id);
-                                          //
-                                          //       print(indexRemove);
-                                          //
-                                          //       if (indexRemove != -1) {
-                                          //         senderTransfers.removeAt(
-                                          //             indexRemove);
-                                          //       }
-                                          //     }
-                                          // }
-
-                                          /* Get the sender instance from database */
-                                          DatabaseReference senderRef =
-                                          FirebaseDatabase.instance.ref(
-                                              "users/$senderUID");
+                                          currentRef.update({
+                                            "transfers": currentTransfers,
+                                            "currencies": currenciesSend
+                                          });
                                         }
+
+                                        // Get the Sender Data and remove his transfer
+                                        /* Get the sender Iban to match with the uid */
+                                        String senderUID = await
+                                        ibanCodeToUid(
+                                            senderIbanCode.substring(5, 9));
+
+                                        // // /* Get the sender data */
+                                        Map senderData = await getUserMap(senderUID);
+                                        List<dynamic>? senderTransfers = senderData['transfers'];
+                                        Map receiverCurrencies = senderData["currencies"];
+
+                                        if (senderTransfers != null) {
+                                          /* Grab the specific transfer with common transfer_id */
+                                          if (transfer_id != "") {
+                                            dynamic elementToFind;
+                                            senderTransfers.forEach((element) {
+                                              if (element['transfer_id'] == transfer_id) {
+                                                elementToFind = element;
+                                              }
+                                            });
+
+                                            int foundIndex =
+                                              senderTransfers.indexOf(elementToFind, 0);
+
+                                            // Here modify every field you want of the sender
+                                            // (not the current user)
+                                            elementToFind['type'] = "sent";
+                                            receiverCurrencies[currency] -= amount;
+
+                                            senderTransfers[foundIndex] =
+                                                elementToFind;
+                                          }
+                                        }
+
+                                        /* Get the sender instance from database */
+                                        DatabaseReference senderRef =
+                                        FirebaseDatabase.instance.ref(
+                                            "users/$senderUID");
+
+                                        /* Update the new transfer and currencies list */
+                                        senderRef.update({
+                                          "transfers": senderTransfers,
+                                          "currencies": receiverCurrencies
+                                        });
+                                        setState(() {});
                                       },
                                       icon: const Icon(Icons.check)),
                                   IconButton(
-                                      onPressed: () {
+                                      onPressed: () async {
+                                        /* Delete the transactions from database of
+                                        * both users -> sender and receiver */
+
+                                        /* Delete the current user transaction */
+                                        String transfer_id = "";
+                                        List<dynamic>? currentTransfers = data["transfers"];
+                                        if (currentTransfers != null) {
+                                          /* Grab the common transfer_id */
+                                          transfer_id = data["transfers"][index]["transfer_id"];
+
+                                          currentTransfers[index]["type"] = "sent";
+
+                                          /* Get instance of db of the current user*/
+                                          DatabaseReference currentRef = FirebaseDatabase
+                                              .instance.ref(
+                                            "users/${FirebaseAuth.instance
+                                                .currentUser?.uid ?? 'null_'
+                                                'uid'}",
+                                          );
+
+                                          /* Update the new transfer list */
+                                          currentRef.update({
+                                            "transfers": currentTransfers
+                                          });
+                                        }
+
+                                        // Get the Sender Data and remove his transfer
+                                        /* Get the sender Iban to match with the uid */
+                                        String senderIbanCode =
+                                          data['transfers'][index]['iban'];
+
+                                        String senderUID = await
+                                        ibanCodeToUid(
+                                            senderIbanCode.substring(5, 9));
+
+                                        // // /* Get the sender data */
+                                        Map senderData = await getUserMap(senderUID);
+                                        List<dynamic>? senderTransfers = senderData["transfers"];
+                                        if (senderTransfers != null) {
+                                            /* Grab the specific transfer with common transfer_id */
+                                            if (transfer_id != "") {
+                                                dynamic elementToFind;
+                                                senderTransfers.forEach((element) {
+                                                  if (element["transfer_id"] == transfer_id) {
+                                                    elementToFind = element;
+                                                  }
+                                                });
+
+                                                int foundIndex =
+                                                    senderTransfers.indexOf(elementToFind, 0);
+
+                                                elementToFind['type'] = "sent";
+                                                senderTransfers[foundIndex] =
+                                                    elementToFind;
+                                            }
+                                        }
+
+                                        /* Get the sender instance from database */
+                                        DatabaseReference senderRef =
+                                        FirebaseDatabase.instance.ref(
+                                            "users/$senderUID");
+
+                                        /* Update the new transfer list */
+                                        senderRef.update({
+                                          "transfers": senderTransfers
+                                        });
+
                                         setState(() {});
                                       },
                                       icon: const Icon(Icons.cancel)),
                                 ],
                               ),
                             ),
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              setState(() {});
-                            },
-                            icon: const Icon(Icons.refresh),
                           ),
                         ],
                       )
